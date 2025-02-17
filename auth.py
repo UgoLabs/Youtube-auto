b7b559cea6dbb021a01f64c28857b73f0c6f7e20
UgoLabs <okwumou@gmail.com>
2025-02-17 14:54:06 -0600

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,5 +1,4 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
-from config import GOOGLE_REDIRECT_URI  # import your redirect URI from config
 
 SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 CLIENT_SECRETS_FILE = 'client_secret.json'
@@ -9,10 +8,8 @@ flow = InstalledAppFlow.from_client_secrets_file(
     scopes=SCOPES
 )
 
-# Set the redirect URI directly (instead of reading from flow.client_config)
-flow.redirect_uri = GOOGLE_REDIRECT_URI
+# Run the console-based flow:
+credentials = flow.run_console(prompt='consent')
 
-auth_url, _ = flow.authorization_url(prompt='consent')
-print("Please visit this URL to authenticate:")
-print(auth_url)
+print("Authentication successful!")
 
